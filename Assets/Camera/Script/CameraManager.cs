c7341c4066fb2d6474deeadc060df8fe6895919e
FCA <test.test>
2023-05-31 17:32:40 +0900

    2つのカメラワーク（カメラパラメータ）を滑らかにつないで遷移させるための処理を作成
    アドベンチャーゲームなどで、次に行くべき場所を示したり、仕掛けを操作したことで動いたオブジェクトを一度見せたりするときに便利
    TPSゲームで通常のビューから銃を構えたビューに切り替えるなど、ちょっとしたモード変化にも使える

--- Assets/Camera/Script/CameraManager.cs
+++ Assets/Camera/Script/CameraManager.cs
@@ -8,28 +8,6 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     /// <summary>
-    /// カメラのパラメータ
-    /// </summary>
-    [Serializable]
-    public class Parameter
-    {
-        // 追従対象
-        public Transform trackTarget;
-        // 座標
-        public Vector3 position;
-        // 角度
-        public Vector3 angles = new Vector3(10.0f, 0.0f, 0.0f);
-        // 距離
-        public float distance = 7.0f;
-        // 画角
-        public float fieldOfView = 45.0f;
-        // オフセットの座標
-        public Vector3 offsetPosition = new Vector3(0.0f, 1.0f, 0.0f);
-        // オフセットの角度
-        public Vector3 offsetAngles;
-    }
-
-    /// <summary>
     /// カメラオブジェクト
     /// </summary>
     [SerializeField]
@@ -47,6 +25,8 @@ public class CameraManager : MonoBehaviour
 
     [SerializeField]
     public Parameter parameter;
+
+    public Parameter Param => parameter;
 
     private void Update()
     {
@@ -68,11 +48,7 @@ public class CameraManager : MonoBehaviour
         if(parameter.trackTarget != null)
         {
             // positionパラメータをtrackTargetの座標に上書き
-            // Lerp() を使って簡単なイージングを入れる
-            parameter.position = Vector3.Lerp(
-                a: parameter.position,
-                b: parameter.trackTarget.position,
-                t: Time.deltaTime * 4.0f);
+            UpdateTrackTargetBlend(parameter);
         }
 
         // パラメータを各種オブジェクトに反映する
@@ -88,6 +64,21 @@ public class CameraManager : MonoBehaviour
         camera.transform.localEulerAngles = parameter.offsetAngles;
     }
 
+    /// <summary>
+    /// 被写体をイージングしながら追いかける
+    /// </summary>
+    /// <param name="parameter">カメラのパラメータ</param>
+    private void UpdateTrackTargetBlend(Parameter parameter)
+    {
+        parameter.position = Vector3.Lerp(
+            a: parameter.position,
+            b: parameter.trackTarget.position,
+            t: Time.deltaTime * 4.0f);
+    }
+
+    /// <summary>
+    /// カメラをマウス操作する場合の処理
+    /// </summary>
     private void MouseMode()
     {
         // マウスの動きの差分をカメラの回り込み角度に反映する
@@ -97,4 +88,65 @@ public class CameraManager : MonoBehaviour
 
         parameter.angles += diffAngles;
     }
+}
+
+/// <summary>
+/// カメラのパラメータ
+/// </summary>
+[Serializable]
+public class Parameter
+{
+    // 追従対象
+    public Transform trackTarget;
+    // 座標
+    public Vector3 position;
+    // 角度
+    public Vector3 angles = new Vector3(10.0f, 0.0f, 0.0f);
+    // 距離
+    public float distance = 7.0f;
+    // 画角
+    public float fieldOfView = 45.0f;
+    // オフセットの座標
+    public Vector3 offsetPosition = new Vector3(0.0f, 1.0f, 0.0f);
+    // オフセットの角度
+    public Vector3 offsetAngles;
+
+
+    /// <summary>
+    /// 2つのカメラワークをブレンド遷移する
+    /// </summary>
+    /// <param name="a">開始値</param>
+    /// <param name="b">終了値</param>
+    /// <param name="t">a,b の補間値</param>
+    /// <param name="ret">使用中のカメラパラメータ</param>
+    /// <returns>a,b の補間された値</returns>
+    public static Parameter Lerp(Parameter a, Parameter b, float t, Parameter ret)
+    {
+        ret.position = Vector3.Lerp(a.position, b.position, t);
+        ret.angles = LerpAngles(a.angles, b.angles, t);
+        ret.distance = Mathf.Lerp(a.distance, b.distance, t);
+        ret.fieldOfView = Mathf.Lerp(a.fieldOfView, b.fieldOfView, t);
+        ret.offsetPosition = Vector3.Lerp(a.offsetPosition, b.offsetPosition, t);
+        ret.offsetAngles = LerpAngles(a.offsetAngles, b.offsetAngles, t);
+
+        return ret;
+    }
+
+    /// <summary>
+    /// 角度の補間
+    /// </summary>
+    /// <param name="a">開始値</param>
+    /// <param name="b">終了値</param>
+    /// <param name="t">a,b の補間値</param>
+    /// <returns>a,b の補間された値</returns>
+    private static Vector3 LerpAngles(Vector3 a, Vector3 b, float t)
+    {
+        Vector3 ret = Vector3.zero;
+        ret.x = Mathf.LerpAngle(a.x, b.x, t);
+        ret.y = Mathf.LerpAngle(a.y, b.y, t);
+        ret.z = Mathf.LerpAngle(a.z, b.z, t);
+
+        return ret;
+    }
+
 }

89f2a2b8553e7e3c28d4d375aeb81a66a0bcc22c
FCA <test.test>
2023-05-31 14:11:55 +0900

    DOTweenアセットの追加

---
+++ Assets/Camera/Script/CameraManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// シーンを実行しなくてもカメラワークが反映されるように[ExecuteInEditMode]を付与
+[ExecuteInEditMode]
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
